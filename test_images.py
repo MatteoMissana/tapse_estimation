576bbfa8913a08c7e82e03acf4742b89c8a6cd24
Matteo <matteo.missana@mail.polimi.it>
2025-01-27 10:39:42 +0100

    added first test code

---
+++ test_images.py
@@ -0,0 +1,21 @@
+import h5py
+from utils.plot import view_volume
+
+file = r"C:\Users\User\Desktop\uni_matteo\quinto_anno\tesi_magistrale\data\4DRVQ_Jinyang\voxels\100001.h5"
+
+def print_structure(name, obj):
+    print(name, obj)
+
+
+with h5py.File(file, 'r') as h5_file:
+    h5_file.visititems(print_structure)
+    print(h5_file['FrameInfo']['frameTimes'][:])
+    print(h5_file['FrameInfo']['endDiastole'][()])
+    print(h5_file['FrameInfo']['endSystole'][()])
+    image_0 = h5_file['Input']['grid00'][:]
+
+print(image_0.shape)
+#image_0 = image_0.transpose(2, 1, 0)
+
+# Call the function with your volume
+view_volume(image_0)
---
+++ utils/plot.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+
+def view_volume(volume):
+    """
+    View slices of a 3D volume (shape: H, W, D) using a Matplotlib slider.
+    :param volume: 3D NumPy array (H, W, D)
+    """
+    # Set up the initial plot
+    fig, ax = plt.subplots()
+    plt.subplots_adjust(bottom=0.25)
+
+    # Initial slice index
+    slice_idx = 0
+    img = ax.imshow(volume[:, :, slice_idx], cmap='gray')
+    ax.set_title(f"Slice: {slice_idx}")
+
+    # Set up the slider
+    ax_slider = plt.axes([0.2, 0.1, 0.65, 0.03])  # [x, y, width, height]
+    slider = Slider(ax_slider, 'Slice', 0, volume.shape[2] - 1, valinit=slice_idx, valstep=1)
+
+    # Update function for slider
+    def update(val):
+        idx = int(slider.val)
+        img.set_data(volume[:, :, idx])
+        ax.set_title(f"Slice: {idx}")
+        fig.canvas.draw_idle()
+
+    slider.on_changed(update)
+    plt.show()
