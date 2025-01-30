c6c4de25b064c4d9d19837eb74cdd11f8d7cc280
Matteo <matteo.missana@mail.polimi.it>
2025-01-30 18:08:21 +0100

    added volume_viewer class

--- test_images.py
+++ test_images.py
@@ -3,6 +3,7 @@ from utils.plot import view_volume, visualize_image
 import numpy as np
 from utils.extract_slices import extract_planes
 
+
 file = r"C:\Users\User\Desktop\uni_matteo\quinto_anno\tesi_magistrale\data\4DRVQ_Jinyang\voxels\100001.h5"
 
 def print_structure(name, obj):
@@ -11,20 +12,33 @@ def print_structure(name, obj):
 
 with h5py.File(file, 'r') as h5_file:
     h5_file.visititems(print_structure)
-    image_0 = h5_file['GroundTruth']['grid00'][:]
+    image_0 = h5_file['Input']['grid00'][:]
+    image_1 = h5_file['GroundTruth']['grid00'][:]
 
-#print(image_0.shape)
-#image_0 = image_0.transpose(0,2,1)
+
 
 #view_volume(image_0)
 
+print(image_1.shape)
+print(image_1.max())
+print(np.where(image_1[:,:,200]==True))
+image_superimposed = image_0 + image_1*50
+print(image_superimposed.max())
+image_superimposed = image_superimposed.transpose(2,0,1)
+print(image_superimposed.max())
+view_volume(image_superimposed)
+
+
+'''
+image_final = image_0+mask
+image_final = image_final.transpose(0,2,1)
 line_point = (151, 151, 150)
 line_direction = (0, 0, 1)
 angles = np.linspace(0, 360, 36)  # 36 planes, one every 10 degrees
 
-planes = extract_planes(image_0, line_point, line_direction, angles)
+planes = extract_planes(mask, line_point, line_direction, angles)
 
 planes = np.asarray(planes)
 planes = planes.transpose(1,2,0)
-view_volume(planes)
-
+visualize_image(image_1[:,:, 100])
+'''
--- utils/plot.py
+++ utils/plot.py
@@ -1,34 +1,80 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
+class VolumeViewer:
+    def __init__(self, volume):
+        """
+        Class to visualize a 3D volume and select points with mouse clicks.
 
-def view_volume(volume):
-    """
-    View slices of a 3D volume (shape: H, W, D) using a Matplotlib slider.
-    :param volume: 3D NumPy array (H, W, D)
-    """
-    # Set up the initial plot
-    fig, ax = plt.subplots()
-    plt.subplots_adjust(bottom=0.25)
+        :param volume: 3D NumPy array (H, W, D)
+        """
+        self.volume = volume
+        self.slice_idx = 0  # Initial slice index
+        self.clicked_points = []  # List to store selected points
 
-    # Initial slice index
-    slice_idx = 0
-    img = ax.imshow(volume[:, :, slice_idx], cmap='gray')
-    ax.set_title(f"Slice: {slice_idx}")
+        # Create the figure
+        self.fig, self.ax = plt.subplots()
+        plt.subplots_adjust(bottom=0.25)
 
-    # Set up the slider
-    ax_slider = plt.axes([0.2, 0.1, 0.65, 0.03])  # [x, y, width, height]
-    slider = Slider(ax_slider, 'Slice', 0, volume.shape[2] - 1, valinit=slice_idx, valstep=1)
+        # Display the initial slice
+        self.img = self.ax.imshow(self.volume[:, :, self.slice_idx], cmap='gray', vmin=0, vmax=255)
+        self.ax.set_title(f"Slice: {self.slice_idx}")
 
-    # Update function for slider
-    def update(val):
-        idx = int(slider.val)
-        img.set_data(volume[:, :, idx])
-        ax.set_title(f"Slice: {idx}")
-        fig.canvas.draw_idle()
+        # Add a colorbar
+        self.cbar = self.fig.colorbar(self.img, ax=self.ax)
+        self.cbar.set_label("Intensity")
 
-    slider.on_changed(update)
-    plt.show()
+        # Add a slider to navigate through slices
+        ax_slider = plt.axes([0.2, 0.1, 0.65, 0.03])
+        self.slider = Slider(ax_slider, 'Slice', 0, self.volume.shape[2] - 1, valinit=self.slice_idx, valstep=1)
+        self.slider.on_changed(self.update_slice)
+
+        # Connect mouse click event
+        self.fig.canvas.mpl_connect('button_press_event', self.onclick)
+
+    def update_slice(self, val):
+        """Update the displayed slice when the slider value changes."""
+        self.slice_idx = int(self.slider.val)
+        self.img.set_data(self.volume[:, :, self.slice_idx])
+        self.ax.set_title(f"Slice: {self.slice_idx}")
+        self.redraw_points()
+        self.fig.canvas.draw_idle()
+
+    def onclick(self, event):
+        """Handle mouse click to save (x, y, z) coordinates."""
+        if event.inaxes == self.ax:
+            x, y = int(event.xdata), int(event.ydata)
+            z = self.slice_idx
+            self.clicked_points.append((x, y, z))
+            print(f"Point selected: (x={x}, y={y}, z={z})")
+            self.redraw_points()
+
+    def redraw_points(self):
+        """Redraw the selected points on the current slice."""
+        self.ax.clear()
+        self.img = self.ax.imshow(self.volume[:, :, self.slice_idx], cmap='gray', vmin=0, vmax=255)
+        self.ax.set_title(f"Slice: {self.slice_idx}")
+
+        # Draw all selected points in the current slice
+        points_in_slice = [(x, y) for x, y, z in self.clicked_points if z == self.slice_idx]
+        if points_in_slice:
+            x_vals, y_vals = zip(*points_in_slice)
+            self.ax.scatter(x_vals, y_vals, c='red', marker='x', s=50)
+
+        self.fig.canvas.draw_idle()
+
+    def get_selected_points(self):
+        """Return all selected points."""
+        return self.clicked_points
+
+    def reset_points(self):
+        """Reset the list of selected points."""
+        self.clicked_points = []
+        self.redraw_points()
+
+    def show(self):
+        """Display the figure."""
+        plt.show()
 
 
 def visualize_image(image):
